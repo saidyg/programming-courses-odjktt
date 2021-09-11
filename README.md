01344858ef380ddc8da129f9ba2d0fb2a5815e2c
saidyg <saidguernine730@gmail.com>
2021-09-11 22:19:38 +0200

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# programming-courses-odjktt
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/programming-courses-odjktt)
---
+++ index.ts
@@ -0,0 +1,9 @@
+//######################################################################
+//###########################           SSTech       ###################
+//###########################   Programming Courses  ###################
+//######################################################################
+let said1, said2, said3: number;
+said1 = parseInt(prompt('faites attention'));
+said2 = parseInt(prompt('faites attention de toi '));
+said3 = said1 + said2;
+console.log(said3);
